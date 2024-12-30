6de1155dc2eac94fa0aa4669d91f893da40423b1
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2024-12-30 18:00:29 -0300

    Criado logica de filtro para quando o usuario for da Empresa ADM

--- server/controllers/financial.js
+++ server/controllers/financial.js
@@ -24,21 +24,30 @@ const Financial = {
             c.name ASC`
       );
 
+      // Verifica se a empresa é ADM, se for retorna tudo da empresa 1 ITJ, 2 MVLOG e 4 SP
+      const filterCompanie = resultByUser[0].companie_id_headcargo === 7 ? `AND Lhs.IdEmpresa_Sistema IN (1 /*ITJ*/, 2 /*MVLOG*/, 4 /*SP*/)` : `AND Lhs.IdEmpresa_Sistema = ${resultByUser[0].companie_id_headcargo}`
+
       let result = await executeQuerySQL(`SELECT
-         Lhs.IdLogistica_House,
-         Lhs.Numero_Processo,
-         DATEPART(MONTH, Lhs.Data_Abertura_Processo) AS Mes,
-         DATEPART(YEAR, Lhs.Data_Abertura_Processo) AS Ano,
-         Lmo.Total_Recebimento
-      FROM
-         mov_Logistica_House Lhs
-      JOIN
-         mov_Logistica_Moeda Lmo ON Lmo.IdLogistica_House = Lhs.IdLogistica_House
-      WHERE
-         Lmo.IdMoeda = 110 -- BRL
-         AND Lhs.Situacao_Agenciamento NOT IN (7 /* CANCELADO */)
-         AND DATEPART(YEAR, Lhs.Data_Abertura_Processo) = ${actualYear}
-         AND Lhs.IdEmpresa_Sistema = ${resultByUser[0].companie_id_headcargo}
+            Lhs.IdLogistica_House,
+            Lhs.Numero_Processo,
+            Emp.IdEmpresa_Sistema AS IdEmpresa_Sistema,
+            Psa.Nome_Fantasia AS Empresa,
+            DATEPART(MONTH, Lhs.Data_Abertura_Processo) AS Mes,
+            DATEPART(YEAR, Lhs.Data_Abertura_Processo) AS Ano,
+            Lmo.Total_Recebimento
+         FROM
+            mov_Logistica_House Lhs
+         JOIN
+            mov_Logistica_Moeda Lmo ON Lmo.IdLogistica_House = Lhs.IdLogistica_House
+         LEFT OUTER JOIN
+            cad_Empresa Emp ON Emp.IdEmpresa_Sistema = Lhs.IdEmpresa_Sistema
+         LEFT OUTER JOIN
+            cad_Pessoa Psa ON Psa.IdPessoa = Emp.IdPessoa
+         WHERE
+            Lmo.IdMoeda = 110 -- BRL
+            AND Lhs.Situacao_Agenciamento NOT IN (7 /* CANCELADO */)
+            AND DATEPART(YEAR, Lhs.Data_Abertura_Processo) = ${actualYear}
+            ${filterCompanie}
       `);
    
       return result;
@@ -63,6 +72,9 @@ const Financial = {
             c.name ASC`
       );
 
+      // Verifica se a empresa é ADM, se for retorna tudo da empresa 1 ITJ, 2 MVLOG e 4 SP
+      const filterCompanie = resultByUser[0].companie_id_headcargo === 7 ? `` : `AND companie_id_headcargo = ${resultByUser[0].companie_id_headcargo}`
+
       let result = await executeQuery(`SELECT
             companie_id_headcargo,
             value,
@@ -72,7 +84,7 @@ const Financial = {
             goal_financial
          WHERE
             year = ${actualYear}
-            AND companie_id_headcargo = ${resultByUser[0].companie_id_headcargo}
+            ${filterCompanie}
       `);
    
       return result;
