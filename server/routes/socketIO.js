c3815c16183f95e530d0df3c18e4e1516a98ea15
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2025-01-02 14:28:23 -0300

    Criado socket io

--- server/routes/socketIO.js
+++ server/routes/socketIO.js
@@ -1,20 +1,33 @@
-// const { executeQuerySQL } = require('../connect/headCargo');
+const { executeQuerySQL } = require('../connect/sqlServer');
 
+const WebSocket = {
+  lastId: 0,
 
-const WebSocket = (io) => {
-  io.on('connection', (socket) => {
-    console.log('Novo Usuario conectado')
+  Init: async function(io) {
+    // Verifica o ultimo processo gerado e salva no lastId
+    const newResult = await WebSocket.listLastProcess();
+    this.lastId = newResult.IdLogistica_House;
 
-    socket.on('changeTheme', (data) => {
-        console.log('tema trocado')
+    io.on('connection', (socket) => {
+      // A cada 1 segundo verifica o socket io
+      setInterval(async () => {
+        const result = await WebSocket.listLastProcess();
+
+        const id = result.IdLogistica_House;
+
+        // Se o id da nova consulta for diferente do ultimo lastId, irá emitir o io
+        if (id !== WebSocket.lastId) {
+          WebSocket.lastId = id; // O lastId será atualizado com o novo valor consultado
+          io.emit('newProcess');
+        }
+      }, 1000);
     })
+  },
 
-  });
-   
-};
+  listLastProcess: async function() {
+    const result = await executeQuerySQL(`SELECT TOP 1 IdLogistica_House FROM mov_Logistica_House ORDER BY IdLogistica_House DESC`);
+    return result[0];
+  }
+}
 
 module.exports = WebSocket;
-
-
-
-
