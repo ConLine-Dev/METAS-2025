6217a1bb4db8ec3140d12960a1aabc9d3ed331ef
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2025-01-07 16:47:00 -0300

    Criado rota exportation

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const Users = require('./api-users');
 const Financial = require('./api-financial');
 const Importation = require('./api-importation');
+const Exportation = require('./api-exportation');
 
 // Function to set io instance
 const setIO = () => {
@@ -13,6 +14,8 @@ const setIO = () => {
   router.use('/financial', Financial);
   // Use as rotas do arquivo api-importation.js
   router.use('/importation', Importation);
+  // Use as rotas do arquivo api-exportation.js
+  router.use('/exportation', Exportation);
 
   return router;
 };
